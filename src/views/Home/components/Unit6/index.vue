91e3ed866f2b59112b89040a5d9ffed02bf65e52
闻野 <441447212@qq.com>
2025-11-20 15:18:09 +0800

    feat(Home/Unit6): 添加视频播放功能并更新.gitignore
    在Unit6组件中添加视频播放功能，用户点击按钮可播放视频
    更新.gitignore文件，忽略public/zip目录

--- src/views/Home/components/Unit6/index.vue
+++ src/views/Home/components/Unit6/index.vue
@@ -1,20 +1,36 @@
 <script setup>
+import { ref } from 'vue'
+import videoSrc from '@/assets/video/home5.mp4'
 
+const isPlaying = ref(false)
+const videoRef = ref(null)
+
+const playVideo = () => {
+  isPlaying.value = true
+}
 </script>
 
 <template>
   <div>
-    <div class="w-[1300px] mt-[85px] mx-auto">
+    <div class="w-[1300px] mt-[85px] mx-auto rounded-[20px] overflow-hidden">
       <div>
         <div class="relative">
-          <div class="size-full absolute z-10 top-0 left-0 pt-[122px]">
+          <div v-if="!isPlaying" class="size-full absolute z-10 top-0 left-0 pt-[122px]">
             <div class="titleText">Beyond Limits</div>
             <div class="label">
               At Caleaf Tech, we don't settle for what others consider "good enough." We push the boundaries of what's possible, constantly seeking technological breakthroughs to deliver the absolute best. When others say "That's as far as we can go," we say "This is just the beginning." We're committed to making every puff not just delicious, but unforgettable, and every customer experience not just satisfactory, but exceptional.
             </div>
-            <img class="size-[50px] mx-auto mt-[20.25px] cursor-pointer" src="@/assets/img/icon10.png" alt="">
+            <img class="size-[50px] mx-auto mt-[20.25px] cursor-pointer" src="@/assets/img/icon10.png" alt="" @click="playVideo">
           </div>
-          <img class="h-[560px] w-full object-cover" src="@/assets/img/t2.png" alt="">
+          <img v-if="!isPlaying" class="h-[560px] w-full object-cover" src="@/assets/img/t2.png" alt="">
+          <video
+            v-else
+            ref="videoRef"
+            class="h-[560px] w-full object-contain bg-black"
+            :src="videoSrc"
+            controls
+            autoplay
+          ></video>
         </div>
       </div>
     </div>
