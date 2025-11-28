c31c2fdde70d38e33b3c54f8e0e8916bb47ef6fb
闻野 <441447212@qq.com>
2025-11-29 02:53:33 +0800

    style(Product4/Unit12): 优化图片布局和样式代码格式
    调整图片的布局结构，使用绝对定位居中显示
    清理样式代码中的注释并保持一致的格式

--- src/views/Product4/components/Unit12/index.vue
+++ src/views/Product4/components/Unit12/index.vue
@@ -3,24 +3,29 @@
 </script>
 
 <template>
-<div class="h-[880px] flex justify-center items-center bg-[#F8F9FD]">
-  <div class="title">
-    GO
-    <img class="size-[261px]" src="@/assets/img/icon41.png" alt="">
-    Creative
+  <div class="h-[880px] flex justify-center items-center bg-[#F8F9FD]">
+    <div class="title">
+      GO
+      <div class="size-[261px] relative">
+        <div class="size-full absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+          <img class="size-full" src="@/assets/img/icon41.png" alt="">
+        </div>
+      </div>
+      Creative
+    </div>
   </div>
-</div>
 </template>
 
 <style scoped lang="scss">
-.title{
+.title {
   color: #111;
   text-align: center;
   font-family: Roboto;
   font-size: 150px;
   font-style: normal;
   font-weight: 700;
-  line-height: 261px; /* 53.333% */
+  line-height: 261px;
+  /* 53.333% */
   display: flex;
   justify-content: center;
   gap: 32px;
