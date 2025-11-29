786151e7c112b0f9c4333b46a57bb2bfb0b8ba8d
闻野 <441447212@qq.com>
2025-11-29 17:45:52 +0800

    feat(Product3/Unit6): 添加产品4的屏幕图片并更新组件布局
    更新Unit6组件，引入产品4的屏幕图片资源并重新设计布局
    移除旧的占位元素，使用实际产品图片展示
    调整样式以匹配新的设计需求

--- src/views/Product3/components/Unit6/index.vue
+++ src/views/Product3/components/Unit6/index.vue
@@ -1,16 +1,25 @@
 <script setup>
+import screenBg0 from '@/assets/product4/screen-bg-0.png'
+import screenBg1 from '@/assets/product4/screen-bg-1.png'
+import screen1 from '@/assets/product4/screen1.png'
+import screen2 from '@/assets/product4/screen2.png'
+import screen3 from '@/assets/product4/screen3.png'
+import screen4 from '@/assets/product4/screen4.png'
+import screen5 from '@/assets/product4/screen5.png'
+import screen6 from '@/assets/product4/screen6.png'
+
+const list = [screen1,screen2,screen3,screen4,screen5,screen6]
 
 </script>
 
 <template>
-<div class="h-[900px] bg-[#F5F5F5]">
-  <div class="relative scroll-x-auto">
-    <div class="flex gap-x-[84px] -ml-[19px] pt-[126px]">
-      <div :class="{
-        'mx-[50px]':index === 1,
-        '!ml-[calc(61px-86px)]':index === 2
-      }" class="size-[280px] rounded-[40px] bg-[#D9D9D9] flex-shrink-0 relative" v-for="(item,index) in 10">
-        <div v-if="index ===2" class="absolute top-[calc(280px+71px)]">
+  <div class="h-[900px] bg-[#F5F5F5]">
+    <div class="h-full">
+      <div class="mb-[55px] flex justify-center items-start gap-[200px]">
+        <div class="w-[250px] relative ml-[-250px]">
+          <img class="w-full absolute top-0 left-0" :src="screenBg1" alt="">
+        </div>
+        <div class="mt-[126px]">
           <div class="title">
             ULTRA-LARGE SMART DISPLAY
           </div>
@@ -18,17 +27,24 @@
             Design Your Own
           </div>
           <div class="w-[500px] title3">
-            Customize it further with any brand graphics to create your unique brand screen. Drive market trends with a design that breaks the mold and leads the way.
+            Customize it further with any brand graphics to create your unique brand screen. Drive market trends with a
+            design that breaks the mold and leads the way.
           </div>
+        </div>
+      </div>
+
+      <div class="flex gap-x-[150px] ml-[100px] justify-center">
+        <div class="w-[200px] h-[400px] rounded-[30px] flex-shrink-0 relative"
+          v-for="(item,index) in list">
+          <img class="size-full" :src="item" alt="">
         </div>
       </div>
     </div>
   </div>
-</div>
 </template>
 
 <style scoped lang="scss">
-.title{
+.title {
   color: #1CE785;
   font-family: Roboto;
   font-size: 20px;
@@ -36,7 +52,8 @@
   font-weight: 400;
   line-height: normal;
 }
-.title2{
+
+.title2 {
   text-wrap: nowrap;
   color: #111;
   font-family: Roboto;
@@ -46,13 +63,15 @@
   line-height: normal;
   margin-top: 19.55px;
 }
-.title3{
+
+.title3 {
   color: #555;
   font-family: Roboto;
   font-size: 20px;
   font-style: normal;
   font-weight: 400;
-  line-height: 30px; /* 150% */
+  line-height: 30px;
+  /* 150% */
   @apply mt-[37.45px]
 }
 </style>
