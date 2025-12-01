a4e378c876b3cac9197f484fc689c0985007873f
闻野 <441447212@qq.com>
2025-12-01 14:40:17 +0800

    refactor(Technology): 使用CSS类替代v-if条件渲染实现响应式布局

--- src/views/Technology/index.vue
+++ src/views/Technology/index.vue
@@ -1,24 +1,4 @@
-<template>
-  <div class="relative bg-black">
-    <!-- 覆盖图层 -->
-    <!-- <img src="/technology.jpg" alt="Overlay Image" class="pointer-events-none absolute  top-0 left-1/2 -translate-x-1/2 w-full opacity-50"/> -->
-    
-    <Header headerClass="white"/>
-    <Unit1/>
-    <Unit2 v-if="!isMobile"/>
-    <m_Unit2 v-else/>
-    <Unit4 v-if="!isMobile"/>
-    <m_Unit4 v-else/>
-    <Unit5 v-if="!isMobile"/>
-    <m_Unit5 v-else/>
-    <Unit6/>
-    <Unit7/>
-    <Splide4/>
-    <div class="bg-white">
-      <Footer/>
-    </div>
-  </div>
-</template>
+
 
 <script setup>
 import Footer from "@/components/Footer.vue";
@@ -33,21 +13,43 @@ import m_Unit5 from "./components/Unit5/m_index.vue";
 import Unit6 from "./components/Unit6/index.vue";
 import Unit7 from "./components/Unit7/index.vue";
 import Splide4 from "@/components/Splide4/index.vue"
-import { ref, onMounted, onUnmounted } from "vue";
-
-const isMobile = ref(false)
-const checkMobile = () => {
-  isMobile.value = window.innerWidth < 1024
-}
-
-onMounted(() => {
-  checkMobile()
-  window.addEventListener('resize', checkMobile)
-})
-
-onUnmounted(() => {
-  window.removeEventListener('resize', checkMobile)
-})
-
 </script>
 
+<template>
+  <div class="relative bg-black">
+    <!-- 覆盖图层 -->
+    <!-- <img src="/technology.jpg" alt="Overlay Image" class="pointer-events-none absolute  top-0 left-1/2 -translate-x-1/2 w-full opacity-50"/> -->
+
+    <Header headerClass="white" />
+    <Unit1 />
+    <Unit2 class="m1024_hide" />
+    <m_Unit2 class="m1024_block" />
+    <Unit4 class="m1024_hide" />
+    <m_Unit4 class="m1024_block" />
+    <Unit5 class="m1024_hide" />
+    <m_Unit5 class="m1024_block" />
+    <Unit6 />
+    <Unit7 />
+    <Splide4 />
+    <div class="bg-white">
+      <Footer />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+.m1024_block {
+  display: none;
+}
+
+@media screen and (max-width: 1024px) {
+  .m1024_hide {
+    display: none;
+  }
+
+  .m1024_block {
+    display: block;
+  }
+}
+</style>
