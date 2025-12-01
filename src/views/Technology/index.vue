1c1644bad12b4f9f882db2b40191394f33874d92
闻野 <441447212@qq.com>
2025-12-01 14:29:55 +0800

    feat(Technology): 启用Unit2、Unit4和Unit5组件

--- src/views/Technology/index.vue
+++ src/views/Technology/index.vue
@@ -24,11 +24,11 @@
 import Footer from "@/components/Footer.vue";
 import Header from "@/components/Header/index.vue";
 import Unit1 from "./components/Unit1/index.vue";
-// import Unit2 from "./components/Unit2/index.vue";
+import Unit2 from "./components/Unit2/index.vue";
 import m_Unit2 from "./components/Unit2/m_index.vue";
-// import Unit4 from "./components/Unit4/index.vue";
+import Unit4 from "./components/Unit4/index.vue";
 import m_Unit4 from "./components/Unit4/m_index.vue";
-// import Unit5 from "./components/Unit5/index.vue";
+import Unit5 from "./components/Unit5/index.vue";
 import m_Unit5 from "./components/Unit5/m_index.vue";
 import Unit6 from "./components/Unit6/index.vue";
 import Unit7 from "./components/Unit7/index.vue";
