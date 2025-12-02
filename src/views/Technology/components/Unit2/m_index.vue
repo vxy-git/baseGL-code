7fc21d216be19aa0b6f4f70b6d656115a647887d
闻野 <441447212@qq.com>
2025-12-03 04:58:50 +0800

    refactor(Technology/Unit2): 移除未使用的视频引用变量

--- src/views/Technology/components/Unit2/m_index.vue
+++ src/views/Technology/components/Unit2/m_index.vue
@@ -1,18 +1,14 @@
 <script setup>
-import { onMounted, onUnmounted, ref } from 'vue'
 import MediaAsset from '@/components/MediaAsset.vue'
 import l1 from '@/assets/technology/t1/l1.mp4'
 import r1 from '@/assets/technology/t1/r1.jpg'
 import r2 from '@/assets/technology/t1/r2.mp4'
-
-const l1VideoRef = ref(null)
-const r2VideoRef = ref(null)
 </script>
 
 <template>
   <div class="unit2 relative w-[1560px] max-w-full mx-auto c_padding pb-[65px]">
     <div class="shrink-0 w-full m_video">
-      <MediaAsset ref="l1VideoRef" type="video" :src="l1" :autoplay="false" :muted="true" :loop="false"
+      <MediaAsset type="video" :src="l1" :autoplay="false" :muted="true" :loop="false"
         :controls="false" :view-play="true" playsinline class="mediaBox size-full" />
     </div>
     <div
@@ -70,7 +66,7 @@ const r2VideoRef = ref(null)
         <div class="title3 mt-[25px]">
           Most customers have reported bubble issues to us with various devices from different suppliers during oil filling. Our U-shape ceramic design solves this problem perfectly. No more burnt taste caused by clogged bubbles. 
         </div>
-        <MediaAsset ref="r2VideoRef" :src="r2" type="video" class="w-[595px] mt-[66px]" muted
+        <MediaAsset :src="r2" type="video" class="w-[595px] mt-[66px]" muted
           :controls="false" :view-play="true" playsinline alt="" />
       </div>
     </div>
