12487119340c8147bbe735e2b436d41d08d9e4c0
闻野 <441447212@qq.com>
2025-12-03 05:39:56 +0800

    feat(Technology): 添加移动端响应式布局支持
    移除原有的媒体查询样式，改用vue的响应式变量动态切换移动端组件

--- src/views/Technology/index.vue
+++ src/views/Technology/index.vue
@@ -1,8 +1,11 @@
 
 
 <script setup>
+import { onMounted, onUnmounted, ref } from "vue";
 import Footer from "@/components/Footer.vue";
 import Header from "@/components/Header/index.vue";
+import Splide4 from "@/components/Splide4/index.vue";
+import { MOBILE_BREAKPOINT } from "@/composables/fit";
 import Unit1 from "./components/Unit1/index.vue";
 import Unit2 from "./components/Unit2/index.vue";
 import m_Unit2 from "./components/Unit2/m_index.vue";
@@ -12,19 +15,35 @@ import Unit5 from "./components/Unit5/index.vue";
 import m_Unit5 from "./components/Unit5/m_index.vue";
 import Unit6 from "./components/Unit6/index.vue";
 import Unit7 from "./components/Unit7/index.vue";
-import Splide4 from "@/components/Splide4/index.vue"
+
+const isClient = typeof window !== "undefined";
+const isMobile = ref(isClient ? window.innerWidth < MOBILE_BREAKPOINT : false);
+
+const updateIsMobile = () => {
+  if (typeof window === "undefined") return;
+  isMobile.value = window.innerWidth < MOBILE_BREAKPOINT;
+};
+
+onMounted(() => {
+  updateIsMobile();
+  window.addEventListener("resize", updateIsMobile);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("resize", updateIsMobile);
+});
 </script>
 
 <template>
   <div class="relative bg-black">
     <Header headerClass="white" />
     <Unit1 />
-    <Unit2 class="m1024_hide" />
-    <m_Unit2 class="m1024_block" />
-    <Unit4 class="m1024_hide" />
-    <m_Unit4 class="m1024_block" />
-    <Unit5 class="m1024_hide" />
-    <m_Unit5 class="m1024_block" />
+    <Unit2 v-if="!isMobile" />
+    <m_Unit2 v-else />
+    <Unit4 v-if="!isMobile" />
+    <m_Unit4 v-else />
+    <Unit5 v-if="!isMobile" />
+    <m_Unit5 v-else />
     <Unit6 />
     <Unit7 />
     <Splide4 />
@@ -33,20 +52,3 @@ import Splide4 from "@/components/Splide4/index.vue"
     </div>
   </div>
 </template>
-
-<style scoped lang="scss">
-
-.m1024_block {
-  display: none;
-}
-
-@media screen and (max-width: 1024px) {
-  .m1024_hide {
-    display: none;
-  }
-
-  .m1024_block {
-    display: block;
-  }
-}
-</style>
