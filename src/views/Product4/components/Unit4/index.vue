8c1f8172aba67af911f682beb715c85a6a5f8700
闻野 <441447212@qq.com>
2025-12-05 05:49:28 +0800

    fix(Product4/Unit4): 将logo从svg格式替换为png并优化遮罩层布局
    将双视频logo从svg格式改为png格式以提高兼容性
    重构遮罩层布局，使用内联svg替代背景图片实现更好的控制

--- src/views/Product4/components/Unit4/index.vue
+++ src/views/Product4/components/Unit4/index.vue
@@ -6,7 +6,7 @@ import MediaAsset from '@/components/MediaAsset.vue'
 
 const icon33 = '/assets/img/icon33.png'
 const dualVideo = '/assets/product4/DUAL.mp4'
-const dualLogo = import.meta.env.VITE_BASE_URL + 'assets/product4/unit3/logo1.svg'
+const dualLogo = import.meta.env.VITE_BASE_URL + 'assets/product4/unit3/logo1.png'
 
 const sectionRef = ref(null)
 const spacerRef = ref(null)
@@ -170,9 +170,17 @@ onBeforeUnmount(() => {
           <MediaAsset class="size-full object-cover" type="video" :src="dualVideo" :autoplay="false" :muted="true"
             :loop="false" :controls="false" />
         </div>
-        <div ref="maskRef"
+        <!-- <div ref="maskRef"
           class="mask-layer w-[100vw] h-[calc(100vh-96px)] absolute left-1/2 top-1/2 mt-[48px] -translate-x-1/2 -translate-y-1/2"
           :style="{backgroundImage: `url(${dualLogo})`, backgroundOrigin: 'cover', backgroundPosition: 'center'}">
+        </div> -->
+
+        <div ref="maskRef" class="mask-layer w-screen h-screen absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+          <svg class="min-w-full min-h-full absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2" xmlns="http://www.w3.org/2000/svg" width="1920" height="1080" viewBox="0 0 1920 1080" fill="none">
+            <path
+              d="M1920 1080H0V0H1920V1080ZM845.809 561.945C845.809 572.883 848.12 582.095 852.742 589.582C857.43 597.069 863.875 602.701 872.078 606.477C880.346 610.253 889.786 612.141 900.398 612.141C911.01 612.141 920.385 610.253 928.523 606.477C936.727 602.701 943.139 597.069 947.762 589.582C952.449 582.095 954.793 572.883 954.793 561.945V468H925.594V561.945C925.594 568.326 924.585 573.534 922.566 577.57C920.613 581.607 917.749 584.569 913.973 586.457C910.262 588.345 905.737 589.289 900.398 589.289C895.19 589.289 890.665 588.345 886.824 586.457C883.048 584.569 880.151 581.607 878.133 577.57C876.115 573.534 875.105 568.326 875.105 561.945V468H845.809V561.945ZM717 468V610.188H760.652C770.288 610.188 779.077 608.592 787.02 605.402C795.027 602.147 801.896 597.557 807.625 591.633C813.419 585.643 817.879 578.482 821.004 570.148C824.194 561.815 825.789 552.603 825.789 542.512V535.773C825.789 525.617 824.194 516.405 821.004 508.137C817.879 499.803 813.419 492.642 807.625 486.652C801.831 480.663 794.962 476.073 787.02 472.883C779.142 469.628 770.451 468 760.945 468H717ZM964.754 610.188H995.906L1005.74 580.207H1056.85L1066.71 610.188H1097.96L1044.83 468H1017.59L964.754 610.188ZM1111.14 610.188H1202.55V587.336H1140.44V468H1111.14V610.188ZM760.945 490.949C766.609 490.949 771.622 491.926 775.984 493.879C780.346 495.767 784.025 498.599 787.02 502.375C790.014 506.151 792.26 510.806 793.758 516.34C795.32 521.874 796.102 528.286 796.102 535.578V542.512C796.102 552.017 794.734 560.122 792 566.828C789.331 573.534 785.359 578.645 780.086 582.16C774.812 585.611 768.335 587.336 760.652 587.336H746.297V490.949H760.945ZM1049.3 557.258H1013.27L1031.26 502.424L1049.3 557.258Z"
+              fill="#F8F9FD" />
+          </svg>
         </div>
       </div>
     </div>
