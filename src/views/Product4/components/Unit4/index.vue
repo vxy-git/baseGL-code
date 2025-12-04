cde3f3d0a4822783c9882e93400185255a81db85
闻野 <441447212@qq.com>
2025-12-05 06:40:34 +0800

    fix(Product4): 修正dualPNG图片路径引用问题

--- src/views/Product4/components/Unit4/index.vue
+++ src/views/Product4/components/Unit4/index.vue
@@ -3,12 +3,12 @@ import { onMounted, onBeforeUnmount, ref } from 'vue'
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import MediaAsset from '@/components/MediaAsset.vue'
-import dualPNG from '@/assets/dual.png'
+// import dualPNG from '@/assets/dual.png'
 
 const icon33 = '/assets/img/icon33.png'
 const dualVideo = '/assets/product4/DUAL.mp4'
 // const dualPNG = 'logo1.png'
-// const dualPNG = import.meta.env.VITE_BASE_URL + 'assets/product4/unit3/logo1.png'
+const dualPNG = import.meta.env.VITE_BASE_URL + 'assets/product4/unit3/logo1.png'
 
 const sectionRef = ref(null)
 const spacerRef = ref(null)
