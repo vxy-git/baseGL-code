3b19e7d1c003ecce02c4cfdb8ba7ae0b9e65e154
闻野 <441447212@qq.com>
2026-06-17 03:03:31 +0800

    style(Unit7): 调整Unit7组件的specs行布局并优化模板格式
    将specsRow改为column-reverse布局并添加居中对齐，简化模板标签的换行格式

--- src/views/Extro/components/Unit7/index.vue
+++ src/views/Extro/components/Unit7/index.vue
@@ -15,22 +15,15 @@ const unitData = useUnitData(props, unit7Data)
     <h2 class="title">{{ unitData.titleText }}</h2>
     <div class="specsRow relative mt-[58px] flex justify-start gap-[40px]">
       <div class="shrink-0 flex gap-y-[15px] gap-x-[20px] w-[460px] flex-wrap">
-        <div
-          v-for="item in unitData.specs"
-          :key="item.label"
-          class="item flex flex-col items-center justify-center"
-        >
+        <div v-for="item in unitData.specs" :key="item.label" class="item flex flex-col items-center justify-center">
           <div class="label">{{ item.label }}</div>
           <div class="value mt-[9px]">{{ item.value }}</div>
         </div>
       </div>
       <div class="w-full flex flex-col items-center justify-center">
         <div class="h-[414px]">
-          <MediaAsset
-            type="image"
-            :src="unitData.specsImage"
-            class="w-full h-full object-cover rounded-[20px] overflow-hidden"
-          />
+          <MediaAsset type="image" :src="unitData.specsImage"
+            class="w-full h-full object-cover rounded-[20px] overflow-hidden" />
         </div>
         <div class="text7 mt-[26px] -translate-x-[4px] text-center">
           {{ unitData.productName }}
@@ -94,7 +87,9 @@ const unitData = useUnitData(props, unit7Data)
   }
 
   .specsRow {
-    flex-direction: column;
+    flex-direction: column-reverse;
+    justify-content: center;
+    align-items: center;
   }
 }
 </style>
